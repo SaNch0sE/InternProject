b837ffdf4d3065653a07799cdf10e23f19a3ddc9
sanch0se <alex.boiko.2001@gmail.com>
2020-04-22 22:13:34 +0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8080);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
   Logger.log(`Application is running on: ${await app.getUrl()}`,'Bootstrap');
 }
 bootstrap();
